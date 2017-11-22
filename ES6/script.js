c645afdf413ae4c184fb49a89af2ee3154f0cf22
Artur Zinnatullin <azinnatu@e1z2r9p3.42.us.org>
2017-11-21 23:28:20 -0800

    fix

--- ES6/script.js
+++ ES6/script.js
@@ -1,5 +1,20 @@
 window.onload = function() {
 
+          //DEFAULT PARAMETERS
+
+  // function log(num=10){
+  //   console.log(num);
+  // }
+  // log();  //console.logs 10 by default
+  // log(5);  //console.logs 5
+
+  // function logNinja(name="john", belt="Red", age=25){
+  //   console.log("My name is " + name + " and my belt colour is " + belt + " and my age is " + age);
+  // }
+  // logNinja();  //console.logs default parameters
+  // logNinja("Arthur", "white", 30);
+
+
           // CONSTANTS
 
   // const pi = 3.142;
@@ -12,6 +27,7 @@ window.onload = function() {
   // console.log(pi);
   // calcArea(5);
 
+
           // THE LET KEYWORD
 
   // var x = 10;
@@ -23,14 +39,51 @@ window.onload = function() {
   //
   // console.log("outside: " + x);
 
-            // LET vs VAR
-  var items = document.getElementsByTagName("li");
 
-  for(let x = 0; x < items.length; x++){
-    items[x].onclick = function(){
-      console.log(x);
-    }
-  }
+            // LET vs VAR
+
+  //   var items = document.getElementsByTagName("li");
+  //
+  //   var y = 5;
+  //
+  //   for(let x = 0; x < items.length; x++){  //if VAR used, x is always 4(items length)
+  //     items[x].onclick = function(){
+  //       console.log(x);
+  //     }
+  //   }
+  // console.log(y); // VAR is global scope variable, LET is not
+
+
+          // THE SPREAD OPERATOR
+
+    // var nums1 = [1,2,3];
+    // var nums2 = [nums1, 4,5,6];
+    //
+    // console.log(nums2);  // console.logs [Array[3], 4, 5, 6]
+    //
+    // var nums2 = [...nums1, 4,5,6];  //spred operator used
+    //
+    // console.log(nums2);  // console.logs [1, 2, 3, 4, 5, 6]
+
+    // var nums = [3,4,9];
+    //
+    // function addNums(a,b,c){
+    //   console.log(a+b+c);
+    // }
+    //
+    // addNums(nums);  //logs 3,4,9undefinedundefined (expects 3 elements, not 1 array)
+    // addNums(...nums);  //logs 16(sum of nums)
+
+            //TEMPLATE STRINGS
+
+    var temp = 'hello,
+
+     my name is Arthur';
+
+     console.log(temp);
+
+
+
 
 
 }
